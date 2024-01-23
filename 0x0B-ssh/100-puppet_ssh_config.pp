748af7ee320e3b914a9b0e213c91d8415c386675
Tommytinny <tommyteenboi@gmail.com>
2024-01-23 09:13:04 -0500

    added files

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,10 @@
+#  SSH client configuration to use the private key ~/.ssh/school
 exec { 'set-private-key':
   command => 'echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
   path    => ['/bin', '/usr/bin'],
 }
 
+# SSH client configuration to refuse to authenticate using a password
 exec { 'disable-password':
   command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config',
   path    => ['/bin', '/usr/bin'],
