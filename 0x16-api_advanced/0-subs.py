6fcc24c3cdcec5ac8913019c493adabfa1760e70
Tommytinny <tommyteenboi@gmail.com>
2024-03-10 09:17:32 +0100

    fix the bug

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,14 +5,14 @@ import requests
 
 def number_of_subscribers(subreddit):
     """return numbers of subscribers"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = f"http://www.reddit.com/r/{subreddit}/about.json"
     header = {"User-Agent": "My-User-Agent"}
-    r = requests.get(
+    ries = requests.get(
             url,
             headers=header,
             allow_redirects=False
             )
-    if r.status_code >= 300:
+    if res.status_code >= 300:
         return 0
     
-    return r.json().get("data").get("subscribers")
+    return res.json().get("data").get("subscribers")
