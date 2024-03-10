eccb3de96ddbf36f45d1c322ad28a0ba9cca5071
Tommytinny <tommyteenboi@gmail.com>
2024-03-10 09:08:46 +0100

    fix the error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,15 +5,14 @@ import requests
 
 def number_of_subscribers(subreddit):
     """return numbers of subscribers"""
-    try:
-        r = requests.get(
-            "https://www.reddit.com/r/{}/about.json".format(subreddit),
-            headers={"User-Agent": "Custom"},
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    header = {"User-Agent": "My-User-Agent"}
+    r = requests.get(
+            url=url,
+            headers=header,
             allow_redirects=False
             )
-        if r.status_code >= 300:
-            return 0
-        else:
-            return r.json().get("data").get("subscribers")
-    except requests.exceptions.RequestException as e:
+    if r.status_code >= 300:
         return 0
+    else:
+        return r.json().get("data").get("subscribers")
