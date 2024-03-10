11ab20507e5ddcd99c35c53af095c09f90c97a6d
Tommytinny <tommyteenboi@gmail.com>
2024-03-10 08:54:08 +0100

    fix the error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
         url, headers={"User-Agent": "MyCustomUserAgent"},
         allow_redirects=False
         )
-    if r.status_code == 404:
-        return r.json().get("data").get("subscribers")
-    else:
+    if r.status_code => 300:
         return 0
+    else:
+        return r.json().get("data").get("subscribers")
