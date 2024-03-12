4ed4c34457225f34dea17355f16624de1f9e9f79
Tommytinny <tommyteenboi@gmail.com>
2024-03-12 11:36:34 +0100

    fix bugs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,17 +5,17 @@ import requests
 
 def number_of_subscribers(subreddit):
     """return numbers of subscribers"""
-    url = f"http://www.reddit.com/r/{subreddit}/about.json"
-    header = {"User-Agent": "My-User-Agent"}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    header = {'User-agent': 'Google Chrome Version 81.04044.129'}
     res = requests.get(
             url,
             headers=header,
             allow_redirects=False
             )
-    if subreddit is None or not instance(subreddit, str):
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
 
     try:
         return res.json().get("data").get("subscribers")
-    except Exception as e:
+    except Exception:
         return 0
