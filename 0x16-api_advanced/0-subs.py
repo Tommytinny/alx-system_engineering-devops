dd7291fcaf75bc7ead89537e23ee73f7d5e3b30d
Tommytinny <tommyteenboi@gmail.com>
2024-03-17 08:01:53 +0100

    fix bugs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-"""module for Reddit API"""
-import requests
+"""
+module for Reddit API
+"""
+
+
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """return numbers of subscribers"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    header = {'User-agent': 'Google Chrome Version 81.04044.129'}
-    res = requests.get(
-            url,
-            headers=header,
-            allow_redirects=False
-            )
+    """
+    return numbers of subscribers
+    """
+
     if subreddit is None or not isinstance(subreddit, str):
         return 0
+    user_agent = {'User-agent': 'Google Chrome Version 81.04044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent}
+    results = response.json()
 
     try:
-        return res.json().get("data").get("subscribers")
+        return results.get('data').get('subscribers')
+
     except Exception:
         return 0
